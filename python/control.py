455c030a0205940292cc29624b3f8a658c9b4c88
Thomasit99 <thomasverhage@outlook.com>
2022-09-22 13:06:43 +0200

    get speed

--- python/control.py
+++ python/control.py
@@ -9,3 +9,8 @@ drone.get_battery()
 def opdracht1():
     drone.get_battery()
 
+def get_speed_x(self) -> int:
+    return self.get_state_field('vgx')
+
+    def get_speed_y(self) -> int:
+    return self.get_state_field('vgy')
