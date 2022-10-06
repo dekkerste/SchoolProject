eae0f4e76fc1978612a5b166f742c5ace8a9b393
brandinos <brandinios@pm.me>
2022-10-06 09:34:51 +0200

    Changed some code and fixed problems with ajax

--- main.py
+++ main.py
@@ -1,43 +1,54 @@
-from flask import Flask, render_template, request
+#!venv/bin/python3
+import json
+
+import cv2
+import numpy as np
+import requests as requests
+from flask import Flask, render_template, request, flash, redirect, Response
 from djitellopy import tello
+# Imports the drone commands. This is used as a shortcut from control.py
+try:
+    from python.control import drone
+except ImportError:
+    print('Error while trying to import "control.py"')
 
 app = Flask(__name__)
+app.secret_key = '1234'
 
 
 @app.route("/", methods=['GET', 'POST'])
-def index(message=None):  # the message var needs to be set to zero
-    if request.method == 'POST':
-        if request.form.get('Connect') == 'Connect':
-            print("Connecting to the drone...")
-            message = 'Connecting to the drone...'  # This message will be shown in the webpage
-            # try to run the drone connection. If it fails, shows it on the webpage.
-            try:
-                # connect()  # Starts a def where the drone connection code is
-                print('i want to connect to the drone')
-            except:
-                print("Connection error")
-                message = "Connection Error"
-            pass
-        elif request.form.get('TakeOff') == 'TakeOff':
-            # pass # do something else
-            print("Taking off the drone")
-            message = "Taking of the drone..."
-            pass
-        else:
-            return render_template("index.html")  # returns the same index.html if the buttons are not clicked
-    elif request.method == 'GET':
-        # return render_template("index.html")
-        print("No Post Back Call")
-    return render_template("index.html", message=message)
+def index():  # the message var needs to be set to zero
+    return render_template("index.html")
 
 
+@app.route('/connect', methods=['GET', 'POST'])
+def connect():
+    print('connecting to drone')
+    try:
+        drone.connect()
+        print('The drone is connected!')
+    except:
+        print('something went wrong')
+    return render_template("index.html")
 
-# Code block that holds the drone connection code.
-def connect(message=None):
-    drone = tello.Tello()
-    drone.connect()
-    print('The drone is connected!')
+
+@app.route('/takeoff', methods=['GET', 'POST'])
+def takeoff():
+    print('taking off the drone')
+    try:
+        drone.takeoff()
+        print('The drone has taken off')
+    except:
+        print('something went wrong')
+    return render_template("index.html")
+
+
+@app.route('/customcommand', methods=['GET', 'POST'])
+def customcommand():
+    cust = request.form['customcommandcontents']
+    print(cust)
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+    app.run(host="127.0.0.1", port=8000, debug=True)
--- python/control.py
+++ python/control.py
@@ -1,39 +1,49 @@
 from djitellopy import Tello
 
-drone = Tello()
-drone.connect()
+try:
+    drone = Tello()
+except:
+    print('something went wrong setting drone var')
 
-drone.takeoff()
-drone.get_battery()
+# drone.get_battery()
+
 
 def speed():
     print(drone.get_speed_x())
     print(drone.get_speed_y())
 
+
 def emergency():
     drone.emergency()
+
 
 def battery():
     print(drone.get_battery())
 
+
 def move_up():
     drone.move_up()
+
 
 def move_down():
     drone.move_down()
 
+
 def move_left(y: int):
     drone.move_left(y)
+
 
 def move_right(x: int):
     drone.move_right(x)
 
+
 def flip_left():
     drone.flip_left()
+
 
 def flip_right():
     drone.flip_right()
 
-move_right(180)
-move_left(180)
-drone.land()
+# move_right(180)
+# move_left(180)
+# drone.land()
