d1955ec1a4475276708fd239bb10f72913279ce9
brandinos <brandinios@pm.me>
2022-10-10 19:29:53 +0200

    Improved ajax and error logging

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 import cv2
 import numpy as np
 # import requests as requests
-from flask import Flask, render_template, request, flash, redirect, Response
+from flask import Flask, render_template, request, flash, redirect, Response, url_for
 from djitellopy import tello
 # Imports the drone commands. This is used as a shortcut from control.py
 
@@ -12,9 +12,11 @@ from djitellopy import tello
 app = Flask(__name__)
 app.secret_key = '1234'
 
+#@BUG
 
 @app.route("/", methods=['GET', 'POST'])
 def index():  # the message var needs to be set to zero
+    # request.script_root = url_for('index', _external=True)
     return render_template("index.html")
 
 
@@ -25,7 +27,7 @@ def connect():
         return {"message": "the drone is connected."}
     except:
         # Returns a JSON object which is later used in AJAX to display a message on the webpage.
-        return {"message": "Something went wrong connecting to the drone."}
+        return {"message": "[ERROR]: Something went wrong connecting to the drone."}
 
 @app.route('/takeoff', methods=['GET', 'POST'])
 def takeoff(drone=None):
@@ -45,5 +47,7 @@ def customcommand():
     return render_template("index.html")
 
 
-if __name__ == '__main__':
+# set export FLASK_APP=main.py before running
+if __name__ == "__main__":
+    print('[INFO]' + ' ' + 'main.py is running')
     app.run(host="127.0.0.1", port=8000, debug=True)
--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,34 +1,49 @@
 // https://www.w3schools.com/js/js_ajax_intro.asp
 // AJAX fixes the problem that the page reloads.
 
+// @TODO shows whats needs to be done. Open the TODO tool window: View > Tool Windows > TODO.
+// https://www.jetbrains.com/help/pycharm/using-todo.html#add_pattern_filter_todo
 
 $(document).ready(function () { //Executes when the page is fully loaded.
-
-    var datastring = $(this).serialize();
+    // https://www.w3schools.com/js/js_const.asp
+    const datastring = $(this).serialize();
 
     // Connect drone
-    $('button#connect').bind('click', function (e) {
+    // binds to a button onclick event
+    $('button#connect').bind('click', function (e) /* e (event) = e.preventDefault(); event.preventDefault */ {
         // prevents the default action that belongs to the event, like page reload, event calls
+        // 'e' is defined in the button bind function. This is linked to prevent default
         e.preventDefault();
         $.ajax({
             contentType: 'application/json;charset=UTF-8',
             // The flask route to be called upon
             type: 'POST',
-            url: '/connectt',
+            // The flask route the data should be submitted to
+            // @app.route('/connect', methods=['GET', 'POST'])
+            url: $SCRIPT_ROOT + '/connect',
             data: datastring,
             // Returns the result as JSON
+            // https://www.w3schools.com/js/js_json_syntax.asp
             dataType: "json",
             // function that will run if ajax has run with no errors
             success: function (response_data) {
                 // Logs the JSON object to the browser console for debug purposes
+                console.log("[DEBUG] " + "JSON " + response_data + " of message");
                 console.log(response_data);
-                // Updates the html with a message. We use JSON here, so we need to call the first layer message
-                document.getElementById("message").innerHTML = response_data.message;
+                // Create a fixed let with the response_data json content
+                let obj = response_data;
+                // Updates the html with a message. We use JSON here.
+                // response_data is defined in a let obj. We select the message content with obj.message
+
+                //@TODO: maybe add a way to give a error a red message. Can be done making an other json object
+                document.getElementById("message").innerHTML = obj.message;
             },
             error: function (request, error) {
                 // Pops out an alert box if AJAX fails to run. If you rename the ajax post url to something that does
                 // not exist, you get the alert error message.
-                alert("request failed");
+                // We use return {"message": "Something went wrong connecting to the drone."}
+                // response_data is equal to the 0 layer of the json string, so message.
+                alert("[ERROR]: AJAX request failed! A typical skill issue...");
             }
         })
     });
